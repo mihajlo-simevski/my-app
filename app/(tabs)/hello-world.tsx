ae5c9f1369ad06090fb420dc301633658837fc2b
Mihajlo Simevski <msimevskii@gmail.com>
2025-03-21 13:55:32 +0100

    feat: removed harcoded values

--- app/(tabs)/hello-world.tsx
+++ app/(tabs)/hello-world.tsx
@@ -1,9 +1,11 @@
+import { useThemeColor } from "@/hooks/useThemeColor";
 import { Text, View } from "react-native";
 
 export default function HelloWorld() {
+  const textColor = useThemeColor({}, "text");
 	return (
 		<View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-			<Text style={{ color: "#fff" }}>Hello World</Text>
+			<Text style={{ color: textColor }}>Hello World</Text>
 		</View>
 	);
 }
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,9 +1,11 @@
 import { Text, View } from "react-native";
+import { useThemeColor } from "@/hooks/useThemeColor";
 
 export default function HomeScreen() {
+	const textColor = useThemeColor({}, "text");
 	return (
 		<View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-			<Text style={{ color: "#fff" }}>Home Screen</Text>
+			<Text style={{ color: textColor }}>Home Screen</Text>
 		</View>
 	);
 }
